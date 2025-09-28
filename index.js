c2e4daf9ba9337e8f55145718dbe3e38113627ac
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-09-27 19:09:53 -0600

    Connected Database

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
+const Client = require("pg").Client;
+require("dotenv").config();
+
 const app = express();
 const port = 3000;
 
+const client = new Client({
+  user: "ecom_user",
+  password: process.env.DB_PASSWORD,
+  host: "localhost",
+  database: "e_commerce",
+  port: 5432,
+});
+
+client.connect();
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  client.query("SELECT * FROM products", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving products");
+    } else {
+      res.json(result.rows);
+    }
+  });
 });
 
 app.listen(port, () => {
