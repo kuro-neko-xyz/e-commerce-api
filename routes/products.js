3c29a8144a19959eee34bc06cf8cc1789371aad5
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-10-11 01:52:45 -0600

    Created GET /products Route

--- routes/products.js
+++ routes/products.js
@@ -2,7 +2,21 @@ const express = require("express");
 
 const producstsRouter = express.Router();
 
-producstsRouter.get("/", (req, res) => {});
+producstsRouter.get("/", async (req, res) => {
+  if (!req.user) {
+    return res.status(403).send("Forbidden");
+  }
+
+  try {
+    const client = req.app.locals.client;
+    const response = await client.query("SELECT * FROM products");
+
+    return res.json(response.rows);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 producstsRouter.get("/:id", (req, res) => {});
 
