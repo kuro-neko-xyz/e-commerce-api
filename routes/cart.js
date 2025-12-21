cf4c56460959a2530cba84d4974bd32d031e1091
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-12-21 15:43:34 -0600

    Added Cart Checkout Route

--- routes/cart.js
+++ routes/cart.js
@@ -113,6 +113,46 @@ cartRouter.get("/view", async (req, res) => {
   }
 });
 
-cartRouter.post("/checkout", (req, res) => {});
+cartRouter.post("/checkout", async (req, res) => {
+  if (!req.user) {
+    res.status(403).send("Forbidden");
+  }
+
+  try {
+    const client = req.app.locals.client;
+    const userId = req.user.id;
+
+    let cart = await client.query(
+      "SELECT * FROM carts WHERE user_id = $1 AND is_active = TRUE",
+      [userId]
+    );
+
+    if (cart.rows.length === 0) {
+      return res.status(400).send("No active cart to checkout");
+    }
+
+    const cartItems = await client.query(
+      "SELECT * FROM cart_items WHERE cart_id = $1",
+      [cart.rows[0].id]
+    );
+
+    // todo: add negative stock check here
+
+    for (let item of cartItems.rows) {
+      await client.query(
+        "UPDATE products SET stock = stock - $1 WHERE id = $2",
+        [item.quantity, item.product_id]
+      );
+    }
+
+    await client.query("UPDATE carts SET is_active = FALSE WHERE id = $1", [
+      cart.rows[0].id,
+    ]);
+
+    return res.status(200).send("Checkout successful");
+  } catch (err) {
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = cartRouter;
