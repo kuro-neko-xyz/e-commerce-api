0c94b4f010c723bbb6bb47f8e075df1094d7a3f4
kuro-neko.xyz <a.aleman.h@icloud.com>
2025-12-21 19:47:46 -0600

    Added Order Routes

--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const ordersRouter = express.Router();
 
-ordersRouter.get("/", (req, res) => {
+ordersRouter.get("/", async (req, res) => {
   if (!req.user) {
     return res.status(403).send("Forbidden");
   }
@@ -10,12 +10,43 @@ ordersRouter.get("/", (req, res) => {
   try {
     const client = req.app.locals.client;
     const userId = req.user.id;
+
+    const orders = await client.query(
+      "SELECT * FROM carts c JOIN orders o ON o.cart_id = c.id WHERE c.user_id = $1",
+      [userId]
+    );
+
+    return res.status(200).json(orders.rows);
   } catch (err) {
     console.log(err);
     return res.status(500).send("Internal Server Error");
   }
 });
 
-ordersRouter.get("/:id", (req, res) => {});
+ordersRouter.get("/:id", async (req, res) => {
+  if (!req.user) {
+    return res.status(403).send("Forbidden");
+  }
+
+  try {
+    const client = req.app.locals.client;
+    const userId = req.user.id;
+    const orderId = req.params.id;
+
+    const order = await client.query(
+      "SELECT * FROM carts c JOIN orders o ON o.cart_id = c.id WHERE o.id = $1 AND c.user_id = $2",
+      [orderId, userId]
+    );
+
+    if (order.rows.length === 0) {
+      return res.status(404).send("Order not found");
+    }
+
+    return res.status(200).json(order.rows[0]);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
 module.exports = ordersRouter;
